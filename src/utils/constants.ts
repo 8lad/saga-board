772cdef1b95e5437fe9fb4fc6510ee0a54faa56e
Vlad <vlasisla1988@gmail.com>
2024-04-02 17:08:06 +0300

    main: Changed the images extension

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,12 +7,12 @@ import sixthImg from "../assets/images/cardImages/6.jpg";
 import seventhImg from "../assets/images/cardImages/7.jpg";
 import eightImg from "../assets/images/cardImages/8.jpg";
 
-import firstImgSlider from "../assets/images/slider/1.jpg";
-import secondtImgSlider from "../assets/images/slider/2.jpg";
-import thirdImgSlider from "../assets/images/slider/3.jpg";
-import fouthImgSlider from "../assets/images/slider/4.jpg";
-import fifthImgSlider from "../assets/images/slider/5.jpg";
-import sixthImgSlider from "../assets/images/slider/6.jpg";
+import firstImgSlider from "../assets/images/slider/1.webp";
+import secondtImgSlider from "../assets/images/slider/2.webp";
+import thirdImgSlider from "../assets/images/slider/3.webp";
+import fouthImgSlider from "../assets/images/slider/4.webp";
+import fifthImgSlider from "../assets/images/slider/5.webp";
+import sixthImgSlider from "../assets/images/slider/6.webp";
 
 export const imgArray = [
   firstImg,
